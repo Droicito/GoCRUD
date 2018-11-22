1d6524e6b5c8a44948157f7079d69a1b35d7aa2d
Droicito <migueleyen@gmail.com>
2018-11-21 20:06:31 -0500

    3er snapshot:Bringin Server Data to Output cmd -->

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 )
+
+type Emp struct{
+	Name string `json:"name"`
+}
 func main(){
 
 	fmt.Println("GO - MSQL Server Connection!")
@@ -13,10 +17,26 @@ func main(){
 	}
 	fmt.Println("Connected Succesfull!")
 	defer db.Close()
+	/*
 	insert,err:=db.Query("insert into employee(name,city) values('cesar','callao'),('miguel','lima');")
 	if err !=nil{
 		panic(err.Error())
 	}
 	fmt.Println("Succesfull insert employees for goblog db!")
 	defer insert.Close()
+	*/
+	results,err:=db.Query("select name from employee;")
+	if err !=nil{
+		panic(err.Error())
+	}
+	fmt.Println("Bringing Server data to Output cmd -->")
+	for results.Next(){
+		var emp Emp
+		err=results.Scan(&emp.Name)
+		if err !=nil{
+			panic(err.Error())
+		}
+		
+		fmt.Println(emp.Name)
+	}
 }
