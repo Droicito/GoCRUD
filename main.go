c8c4923ca7f5f3112f09c304f0353ceaa95813db
Droicito <migueleyen@gmail.com>
2018-11-24 16:18:23 -0500

    Web Brings Mysql data and shows it in localhost Browser

--- main.go
+++ main.go
@@ -2,12 +2,41 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
-func main(){
-	http.HandleFunc("/",handler)
-	http.ListenAndServe(":8080",nil)
+func checkErr(err error){
+	if err !=nil{
+		panic(err)
+	}
+}
+func dbConn()(db *sql.DB){
+	dbDriver:="mysql"
+	dbUser:="root"
+	dbPass:="root"
+	dbName:="goblog"
+	db,err:=sql.Open(dbDriver,dbUser+":"+dbPass+"@/"+dbName)
+	checkErr(err)
+	return db
 }
 
-func handler(w http.ResponseWriter,r *http.Request){
-	fmt.Fprintf(w,"Hello World - Youtube is amazing!")
+func main(){
+	http.HandleFunc("/",Index)
+	http.ListenAndServe(":8080",nil)
 }
+func Index(w http.ResponseWriter,r *http.Request){
+	db:=dbConn()
+	selDB,err:=db.Query("select * from employee order by id desc;")
+	checkErr(err)
+	for selDB.Next(){
+		var id int
+		var name,city string
+		err=selDB.Scan(&id,&name,&city)
+		checkErr(err)
+		fmt.Fprintln(w,id)
+		fmt.Fprintln(w,name)
+		fmt.Fprintln(w,city)
+	}
+	defer db.Close()
+}
+
