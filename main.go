114a2dfc4adce15661c5942ef209eca04e549026
Droicito <migueleyen@gmail.com>
2018-11-25 17:24:29 -0500

    Web bring data in html index

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 import (
-	"fmt"
+//	"fmt"
 	"net/http"
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"html/template"
 )
+type Employee struct{
+	Id int
+	Name string
+	City string
+}
+
 func checkErr(err error){
 	if err !=nil{
 		panic(err)
@@ -24,19 +31,24 @@ func main(){
 	http.HandleFunc("/",Index)
 	http.ListenAndServe(":8080",nil)
 }
+var tmpl=template.Must(template.ParseGlob("form/*.html"))
 func Index(w http.ResponseWriter,r *http.Request){
 	db:=dbConn()
 	selDB,err:=db.Query("select * from employee order by id desc;")
 	checkErr(err)
+	emp:=Employee{}
+	res:=[]Employee{}
 	for selDB.Next(){
 		var id int
 		var name,city string
 		err=selDB.Scan(&id,&name,&city)
 		checkErr(err)
-		fmt.Fprintln(w,id)
-		fmt.Fprintln(w,name)
-		fmt.Fprintln(w,city)
+		emp.Id=id
+		emp.Name=name
+		emp.City=city
+		res=append(res,emp)
 	}
+	tmpl.ExecuteTemplate(w,"index.html",res)
 	defer db.Close()
 }
 
